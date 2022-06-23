ffab9037a31a3df5af36b14d5a1e3f2ced15eaeb
Vinicius L S Braz <vinicius_lsb@live.com>
2022-06-22 23:04:38 -0300

    FEAT: User info step done

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,4 +1,7 @@
 const State = require('../models/State');
+const User = require('../models/User');
+const Category = require('../models/Category');
+const Ad = require('../models/Ad');
 
 class UserController {
   async getStates(request, response) {
@@ -6,8 +9,26 @@ class UserController {
     response.json({ states });
   }
 
-  async info() {
-    return null;
+  async info(request, response) {
+    const { token } = request.query;
+
+    const user = await User.findOne({ token });
+    const state = await State.findById(user.state);
+    const ads = await Ad.find({ idUser: user._id.toString });
+
+    const adList = [];
+    Object.keys(ads).forEach(async (key) => {
+      const categoryType = await Category.findById(adList[key].category);
+
+      adList.push({ ...ads[key], category: categoryType.slug });
+    });
+
+    response.json({
+      name: user.name,
+      email: user.email,
+      state: state.name,
+      ads: adList,
+    });
   }
 
   async update() {
