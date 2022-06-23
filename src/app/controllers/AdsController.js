2d5a7396e571459bcdfafb4b8be68f727243db1b
Vinicius L S Braz <vinicius_lsb@live.com>
2022-06-23 14:21:17 -0300

    FEAT: default categories implemented and method to list them

--- src/app/controllers/AdsController.js
+++ src/app/controllers/AdsController.js
@@ -1,3 +1,5 @@
+const Category = require('../models/Category');
+
 class AdsController {
   async index() {
     return null;
@@ -15,8 +17,17 @@ class AdsController {
     return null;
   }
 
-  async getCategories() {
-    return null;
+  async getCategories(request, response) {
+    const getCategories = await Category.find();
+
+    const categories = getCategories.map((category) => (
+      {
+        ...category._doc,
+        img: `${process.env.BASE}/assets/images/${category.slug}.png`,
+      }
+    ));
+
+    response.json({ categories });
   }
 }
 
